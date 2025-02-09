2cdd4ec74cdc39e139cd5fbdfa11700c747c27a5
Yasmine <yasmineelmenofy@gmail.com>
2025-02-09 19:01:49 +0200

    Stack implementation using array

---
+++ Data_Structure/Stack/Arraybased_Stack.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Stack.h"
+
+void CreateStack(Stack *s){
+    s->top=-1;
+}
+
+int Is_StackFull(Stack *s){
+ if(s->top == MAX_STACK-1)
+    return 1;
+  else
+ return 0;
+}
+
+int Is_StackEmpty(Stack *s){
+  if(s->top == -1)
+    return 1;
+  else
+    return 0;
+}
+
+int StackSize(Stack *s){
+ return s->top +1;
+}
+
+void push(Stack *s,Stack_Element data){
+ if(Is_StackFull(s)){
+    printf("Stack overflow \n");
+    return;
+ }else{
+     s->top ++;
+  s->arr[(s->top)]=data;
+ }
+}
+
+Stack_Element pop(Stack *s){
+ if(Is_StackEmpty(s)){
+    printf("Stack under flow \n" );
+    return -1;
+ }else{
+   Stack_Element data;
+   data = s->arr[(s->top)--];
+   return data;
+ }
+}
+
+void ClearStack(Stack *s){
+  s->top=-1;
+}
+
+void TraverseStack(Stack *s){
+ if(Is_StackEmpty(s)){
+    printf("Stack under flow \n" );
+    return;
+ }else{
+    for (int i = s->top; i >= 0; i--) {
+        printf("%d ", s->arr[i]);
+    }
+    printf("\n");
+}
+}
---
+++ Data_Structure/Stack/Arraybased_Stack.h
@@ -0,0 +1,22 @@
+#ifndef ARRAYBASED_STACK_H_INCLUDED
+#define ARRAYBASED_STACK_H_INCLUDED
+
+#define MAX_STACK  100
+typedef int Stack_Element;
+
+typedef struct {
+ Stack_Element arr[MAX_STACK];
+ int top;
+}Stack;
+
+void CreateStack(Stack *s);
+int Is_StackFull(Stack *s);
+int Is_StackEmpty(Stack *s);
+int StackSize(Stack *s);
+void push(Stack *s, Stack_Element data);
+Stack_Element pop(Stack *s);
+void ClearStack(Stack *s);
+void TraverseStack(Stack *s);
+
+
+#endif // ARRAYBASED_STACK_H_INCLUDED
